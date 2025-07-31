bd8398871505b833f2c4ed11ccd2d59ec876dfaa
CrowdConnected CI <ci@crowdconnected.com>
2025-07-31 14:01:04 +0100

    Version 2.1.3

---
+++ CrowdConnectedGeo.xcframework/ios-arm64_x86_64-simulator/CrowdConnectedGeo.framework/Headers/CrowdConnectedGeo.h
@@ -0,0 +1,15 @@
+//
+//  CrowdConnectedGeo.h
+//  CrowdConnectedGeo
+//
+//  Created by TCode on 18/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CrowdConnectedGeo.
+FOUNDATION_EXPORT double CrowdConnectedGeoVersionNumber;
+
+//! Project version string for CrowdConnectedGeo.
+FOUNDATION_EXPORT const unsigned char CrowdConnectedGeoVersionString[];
+
